5e10bde1c233c6799aeb07b39fa3d0cc8d3ae0f7
Tomas Rajevic <tomaspolp@gmail.com>
2026-03-18 18:52:36 -0300

    fix(user):Deleted else statement

--- usuario.py
+++ usuario.py
@@ -12,5 +12,3 @@ class Usuario:
         for tarea in self.tareas:
             if tarea.estaLista():
                 print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
