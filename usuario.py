acdae21288a8d8a94b8b73d4b66afa934ce4804b
xekiyo1 <xekiyo12@gmail.com>
2026-03-18 18:59:05 -0300

    feat: sigma toilet

--- usuario.py
+++ usuario.py
@@ -9,8 +9,7 @@ class Usuario:
         self.tareas.append(tarea)
 
     def listarTareas(self):
-        for tarea in self.tareas:
-            if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
+       for tarea in self.tareas:
+           if tarea.estaLista():
+               print(f"La tarea {tarea.obtenerNombre()} está lista")
+               print(f"La tarea {tarea.obtenerNombre()} no está lista")
