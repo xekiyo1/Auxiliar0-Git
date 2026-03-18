4b6bac0b9f073e9978edc8ae4118e05f5f79eada
Tomas Rajevic <tomaspolp@gmail.com>
2026-03-18 18:49:51 -0300

    feat(user):Added new methods

--- usuario.py
+++ usuario.py
@@ -7,3 +7,10 @@ class Usuario:
 
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
+
+    def listarTareas(self):
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"[X] {tarea.obtenerNombre()}" )
+            else:
+                print(f"[ ] {tarea.obtenerNombre()}" )
